148e538e4c5138d5f059d51f083f7e149faa81d2
920999181 <920999181@qq.com>
2017-01-14 15:11:17 +0800

    Signed-off-by: 920999181 <920999181@qq.com>

---
+++ PinYinHelper/PinYinUtil.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.International.Converters.PinYinConverter;
+
+namespace PinYinHelper
+{
+    public class PinYinUtil
+    {
+        //返回字符串的简拼  
+        public static string GetSimplePinYin(string inputTxt)
+        {
+            string shortR = "";
+            foreach (char c in inputTxt.Trim())
+            {
+                ChineseChar chineseChar = new ChineseChar(c);
+                shortR += chineseChar.Pinyins[0].Substring(0, 1).ToLower();
+            }
+            return shortR;
+        }
+
+        //返回字符串全拼  
+        public static string GetAllPinYin(string inputTxt)
+        {
+            string allR = "";
+            foreach (char c in inputTxt.Trim())
+            {
+                ChineseChar chineseChar = new ChineseChar(c);
+                allR += chineseChar.Pinyins[0].Substring(0, chineseChar.Pinyins[0].Length - 1).ToLower();
+            }
+            return allR;
+        }
+    }
+}
---
+++ PinYinHelper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PinYinHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("上海：" + PinYinUtil.GetAllPinYin("上海"));
+
+            #region 有bug
+            Console.WriteLine("重庆：" + PinYinUtil.GetAllPinYin("重庆"));
+
+            Console.WriteLine("重量：" + PinYinUtil.GetAllPinYin("重量")); 
+            #endregion
+        }
+    }
+}
